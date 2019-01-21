9c5f858b086a23f15b9350a0cb3f6e78c94e982c
HackerRank <hackerrank@example.com>
2019-01-21 19:35:58 +0000

    Saved at Mon Jan 21 2019 17:35:57 GMT-0200 (Brasilia Summer Time)

--- src/main/java/com/hackerrank/github/model/Actor.java
+++ src/main/java/com/hackerrank/github/model/Actor.java
@@ -3,7 +3,7 @@ package com.hackerrank.github.model;
 @Entity
 public class Actor {
     @Id
-    @GeneratedValue(generationT)
+    @GeneratedValue(generationType=)
     private Long id;
     private String login;
     private String avatar;
